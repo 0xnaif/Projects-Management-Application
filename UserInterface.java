962d961790af49db45d69e77b8b0d9130a4c1ed0
Naif <48796390+naif0x@users.noreply.github.com>
2022-10-17 20:38:01 +0300

    Update UserInterface.java

--- UserInterface.java
+++ UserInterface.java
@@ -1,9 +1,10 @@
 import java.io.IOException;
 
 public class UserInterface {
-
+	private static ProjectManager projectManager = new ProjectManager();
+	
 	public static void main(String[] args) throws IOException {
-		ProjectManager.read();
+		projectManager.read();
 
 	}
 }
