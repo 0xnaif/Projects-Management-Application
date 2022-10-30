0b9253ae05dcbf8a8c8ad55c6bfe9eac8d87b83d
Naif <48796390+naif0x@users.noreply.github.com>
2022-10-30 07:00:09 +0300

    Update Project.java

--- Project.java
+++ Project.java
@@ -1,11 +1,11 @@
 import java.util.ArrayList;
 import java.util.Date;
 
+import stage.Stage;
+
 public class Project {
 	private int numOfStages;
 	private String nodeID, customerID;
-	private Date startDate, endDate;
-	private Date createDate, changeDate;
 	private ArrayList<Stage> stages = new ArrayList<>();
 
 	public Project() {
@@ -34,38 +34,6 @@ public class Project {
 
 	public void setNumOfStages(int numOfStages) {
 		this.numOfStages = numOfStages;
-	}
-
-	public Date getStartDate() {
-		return startDate;
-	}
-
-	public void setStartDate(Date startDate) {
-		this.startDate = startDate;
-	}
-
-	public Date getEndDate() {
-		return endDate;
-	}
-
-	public void setEndDate(Date endDate) {
-		this.endDate = endDate;
-	}
-
-	public Date getCreateDate() {
-		return createDate;
-	}
-
-	public void setCreateDate(Date createDate) {
-		this.createDate = createDate;
-	}
-
-	public Date getChangeDate() {
-		return changeDate;
-	}
-
-	public void setChangeDate(Date changeDate) {
-		this.changeDate = changeDate;
 	}
 
 	public ArrayList<Stage> getStages() {
